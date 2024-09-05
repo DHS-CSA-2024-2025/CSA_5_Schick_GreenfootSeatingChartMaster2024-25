df0d7724ab574e8d119df67fe88bd9a1185e122a
ArnavShah1 <shah3634@mydusd.org>
2024-09-05 13:07:35 -0700

    Updated code ArnavShah

--- ArnavShah.java
+++ ArnavShah.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The KilgoreTrout class can be used as a model for your own class that represents you and your seating location in AP CSA
+ * The ArnavShah class represents a student in the classroom and handles the animations when clicked.
  * 
  * @author Mr. Kaehms
  * @version 2.0 Aug 13, 2019
@@ -9,126 +9,98 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ArnavShah extends Student implements SpecialInterestOrHobby
 {
-
-    /**
-     * Constructor for the KilgoreTrout class.
-     * Constructors are special methods with the same exact name as the class name.  
-     * Constructors to not have return types.
-     * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
-     *  lists to initalize for different conditions (depending on what constructors have been written.
-     * @param String f (firstname)
-     * @param String l (lastname)
-     * @param int r (row of seating arrangement)
-     * @param int s (seat number within row seating arrangement)
-     * 
-     */
+    private static final int flipSteps = 36; // Number of steps for 360-degree flip
+    private static final int resizeSteps = 10; // Number of steps for resizing
+    private static final int Delay = 5; // Delay between animation steps
+    
     public ArnavShah(String f, String l, int r, int s) {
-        firstName=f;
-        lastName=l;
-        mySeatX=r;
-        mySeatY=s;
-        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
-        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+        firstName = f;
+        lastName = l;
+        mySeatX = r;
+        mySeatY = s;
+        portraitFile = f.toLowerCase() + l.toLowerCase() + ".jpg"; // Make sure to name your image files firstlast.jpg, all lowercase!!!
+        //standingFile = firstName.toLowerCase() + lastName.toLowerCase() + "-standing.jpg";
+        //soundFile = f.toLowerCase() + l.toLowerCase() + ".wav"; // Make sure to name your sound files firstlast.wav, all lowercase!!!
         setImage(portraitFile);
-        sitting=true;
+        sitting = true;
     }
-    /**
-     * Default constructor, if you don't pass in a name and seating location
-     * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
-     * of the classroom.
-     */
+    
     public ArnavShah() {
-        firstName="Arnav";
-        lastName="Shah";
-        mySeatX=1;
-        mySeatY=1;
-       // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-       portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-       standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+        firstName = "Arnav";
+        lastName = "Shah";
+        mySeatX = 1;
+        mySeatY = 1;
+        portraitFile = firstName.toLowerCase() + lastName.toLowerCase() + ".jpg";
+        //standingFile = firstName.toLowerCase() + lastName.toLowerCase() + "-standing.jpg";
+        soundFile = firstName.toLowerCase() + lastName.toLowerCase() + ".wav";
         setImage(portraitFile);
-        sitting=true;
+        sitting = true;
     }
     
-     /**
-     * Act - do whatever the KilgoreTrout actor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */   
-    public void act() 
-    {
-        // Add your action code here.
-        if(Greenfoot.mouseClicked(this)){
-          //  if (sitting){
-                sitting=false;
-                setImage(standingFile);
-                System.out.println(""); // Print a blank line to create space between any student output.
-                getName();
-                sayName(soundFile);
+    //@Override
+    public void act() {
+        if (Greenfoot.mouseClicked(this)) {
             
-                myHobby("I like to time travel!");
-            // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
-            // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
-            // Call the sitDown() method to move back  to your seat
+            //setImage(standingFile);
+            getName();
+            sayName(soundFile);
             
-                circleClass();  // Kilgore Trount's special method... Please write one of your own. You can use this, but please modify it and be creative.
-           
-                sitDown();
-            }
-        
-    } 
+            myHobby("I like to run track!");
+            flipAnimation();
+            resizeAnimation();
+            sitDown(); // Returns back to seat after animation
+        }
+    }
     
-    /**
-     * Prints the first and last name to the console
-     */
-    public void getName(){
+    private void flipAnimation() {
+        GreenfootImage originalImage = getImage();
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+        
+        for (int i = 0; i < flipSteps; i++) {
+            GreenfootImage rotatedImage = new GreenfootImage(originalImage);
+            rotatedImage.rotate(i * 360 / flipSteps);
+            setImage(rotatedImage);
+            Greenfoot.delay(Delay);
+        }
+    }
+    
+    public void getName() {
         System.out.println("My name is " + firstName + " " + lastName);
     }
-    /**
-     * This method needs to allow the user to interact with the student through a question and answer interface, and provide
-     * some mechanism that allows the student to sit down once the Q&A session ends.  You can use this basic model, or come up
-     * with some additional class and object that represents a blackboard, or a talking cartoon bubble etc. If you provide extra
-     * classes, make sure to fully document so other students can use the same interface.
-     */
-
-   
-    /**
-     * This is a local method specific to the KilgoreTrout class used to animate the character once the image is clicked on.
-     * You should write your own methods to perform your own animation for your character/avatar.
-     */
-    public void circleClass(){
-        setLocation(0,0);
-         Greenfoot.delay(10);
-        // move right
-        for (int i=1;i<=9;i++){
-            setLocation(i,0);
-            Greenfoot.delay(10);
-        }
-        // move back
-        for (int i=1;i<=5;i++){
-            setLocation(9,i);
-            Greenfoot.delay(10);
-        }      
-         // move left
-        for (int i=9;i>=0;i--){
-            setLocation(i,5);
-            Greenfoot.delay(10);
-        }      
-              // move Forward
-        for (int i=5;i>=0;i--){
-            setLocation(0,i);
-            Greenfoot.delay(10);
-        }   
-           Greenfoot.delay(20);
-           returnToSeat();
+    
+    public void myHobby(String s) {
+        System.out.println(s);
     }
-     /**
-     * myHobby is one of the interfaces provided.  
-     * An interface is just a contract for the methods that you will implement in your code.  The College Board no longer
-     * tests on abstract classes and interfaces, but it is good to know about them
-     */
-     public void myHobby(String s) {
-         System.out.println(s);
-}
+    private void resizeAnimation() {
+        GreenfootImage originalImage = getImage();
+        int originalWidth = originalImage.getWidth();
+        int originalHeight = originalImage.getHeight();
+        
+        // Increase size
+        for (int i = 0; i < resizeSteps; i++) {
+            int newWidth = originalWidth + i * 10;
+            int newHeight = originalHeight + i * 10;
+            GreenfootImage resizedImage = new GreenfootImage(originalImage);
+            resizedImage.scale(newWidth, newHeight);
+            setImage(resizedImage);
+            Greenfoot.delay(Delay);
+        }
+        
+        // Decrease size
+        for (int i = resizeSteps; i > 0; i--) {
+            int newWidth = originalWidth + i * 10;
+            int newHeight = originalHeight + i * 10;
+            GreenfootImage resizedImage = new GreenfootImage(originalImage);
+            resizedImage.scale(newWidth, newHeight);
+            setImage(resizedImage);
+            Greenfoot.delay(Delay);
+        }
+        
+        // Restore original size
+        setImage(originalImage);
+    }
+    // Additional methods for animations or interactions can be added here
+}  
 
-}
+
