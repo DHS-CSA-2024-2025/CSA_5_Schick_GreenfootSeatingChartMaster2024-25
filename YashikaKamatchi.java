f5229f74c80af9e119220556252954d8d027b338
yashikakamatchi <kamatchisoundaram6899@mydusd.org>
2024-09-09 14:03:04 -0700

    Add files via upload

--- YashikaKamatchi.java
+++ YashikaKamatchi.java
@@ -39,14 +39,14 @@ public class YashikaKamatchi extends Student implements SpecialInterestOrHobby
      * of the classroom.
      */
     public YashikaKamatchi() {
-        firstName="Kilgore";
-        lastName="Trout";
-        mySeatX=1;
-        mySeatY=1;
-       // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+        firstName="Yashika";
+        lastName="Kamatchi";
+        mySeatX=5;
+        mySeatY=3;
+       //imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+       soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
         setImage(portraitFile);
         sitting=true;
     }
@@ -66,7 +66,7 @@ public class YashikaKamatchi extends Student implements SpecialInterestOrHobby
                 getName();
                 sayName(soundFile);
             
-                myHobby("I like to time travel!");
+                myHobby("I like listening to music!");
             // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
             // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
             // Call the sitDown() method to move back  to your seat
@@ -99,28 +99,15 @@ public class YashikaKamatchi extends Student implements SpecialInterestOrHobby
     public void circleClass(){
         setLocation(0,0);
          Greenfoot.delay(10);
-        // move right
-        for (int i=1;i<=9;i++){
-            setLocation(i,0);
+
+        int x = 0;
+        int y = 0;
+        for (int i = 1; i<=10; i++) {
+            setLocation(x + i, y + i);
             Greenfoot.delay(10);
         }
-        // move back
-        for (int i=1;i<=5;i++){
-            setLocation(9,i);
-            Greenfoot.delay(10);
-        }      
-         // move left
-        for (int i=9;i>=0;i--){
-            setLocation(i,5);
-            Greenfoot.delay(10);
-        }      
-              // move Forward
-        for (int i=5;i>=0;i--){
-            setLocation(0,i);
-            Greenfoot.delay(10);
-        }   
-           Greenfoot.delay(20);
-           returnToSeat();
+            Greenfoot.delay(20);
+            returnToSeat();
     }
      /**
      * myHobby is one of the interfaces provided.  
