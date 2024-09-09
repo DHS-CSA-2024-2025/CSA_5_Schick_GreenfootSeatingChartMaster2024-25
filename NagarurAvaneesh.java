9caccc14c7e8193ea53cd946d5405f136e2bb0fc
ANagarur <86037780+ANagarur@users.noreply.github.com>
2024-09-08 23:18:50 -0700

    Update NagarurAvaneesh.java

--- NagarurAvaneesh.java
+++ NagarurAvaneesh.java
@@ -29,7 +29,7 @@ public class NagarurAvaneesh extends Student implements SpecialInterestOrHobby
         mySeatY=s;
         portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
         standingFile="avaneeshstand.jpeg";
-        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+        soundFile=f.toLowerCase()+l.toLowerCase()+".m4a";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
         setImage(portraitFile);
         sitting=true;
     }
@@ -46,7 +46,7 @@ public class NagarurAvaneesh extends Student implements SpecialInterestOrHobby
        // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        standingFile="avaneeshstand.jpeg";
-        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".m4a";
         setImage(portraitFile);
         sitting=true;
     }
@@ -65,8 +65,9 @@ public class NagarurAvaneesh extends Student implements SpecialInterestOrHobby
                 System.out.println(""); // Print a blank line to create space between any student output.
                 getName();
                 sayName(soundFile);
+                
             
-                myHobby("I like to time travel!");
+                myHobby("I like to read");
             // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
             // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
             // Call the sitDown() method to move back  to your seat
@@ -102,25 +103,30 @@ public class NagarurAvaneesh extends Student implements SpecialInterestOrHobby
         // move right
         for (int i=1;i<=9;i++){
             setLocation(i,0);
+            spin();
             Greenfoot.delay(10);
         }
         // move back
         for (int i=1;i<=5;i++){
             setLocation(9,i);
+            spin();
             Greenfoot.delay(10);
         }      
          // move left
         for (int i=9;i>=0;i--){
             setLocation(i,5);
+            spin();
             Greenfoot.delay(10);
         }      
               // move Forward
         for (int i=5;i>=0;i--){
             setLocation(0,i);
+            spin();
             Greenfoot.delay(10);
         }   
-           Greenfoot.delay(20);
-           returnToSeat();
+       Greenfoot.delay(20);
+       setRotation(0);
+       returnToSeat();
     }
      /**
      * myHobby is one of the interfaces provided.  
@@ -131,5 +137,8 @@ public class NagarurAvaneesh extends Student implements SpecialInterestOrHobby
          System.out.println(s);
          
 }
+    public void spin(){
+        setRotation(getRotation() + 5);
+    }
 
 }
