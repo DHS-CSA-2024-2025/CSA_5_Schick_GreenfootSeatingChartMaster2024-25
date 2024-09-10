ba16b431278af6396459ac01f33ec94f59280c75
Hussain Kazi <kazi0561@mydusd.org>
2024-09-10 13:53:29 -0700

    Hussain Classroom

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        HussainKazi HussainKazi = new HussainKazi();
+        addObject(HussainKazi,6,6);
+        HussainKazi.assignSeat();
   
     }
     
